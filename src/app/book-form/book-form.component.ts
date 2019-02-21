93f40a22e508d5ac293eca2b0fe8a822814ce9c5
Dusan Gledovic <dusan.gledovic@gmail.com>
2019-02-21 22:23:46 +1000

    added form clearing on submit

--- src/app/book-form/book-form.component.ts
+++ src/app/book-form/book-form.component.ts
@@ -1,7 +1,12 @@
 import Book from '../book';
 import { BookService } from '../book.service';
-import { Component } from '@angular/core';
-import { FormBuilder, Validators } from '@angular/forms';
+import { Component, OnInit } from '@angular/core';
+import {
+  FormBuilder,
+  FormGroup,
+  FormGroupDirective,
+  Validators
+  } from '@angular/forms';
 
 const maxLength = 30;
 
@@ -10,14 +15,10 @@ const maxLength = 30;
   templateUrl: './book-form.component.html',
   styleUrls: ['./book-form.component.css']
 })
-export class BookFormComponent {
-  maxLength = maxLength;
+export class BookFormComponent implements OnInit {
+  maxLength: number = maxLength;
 
-  addBookForm = this.fb.group({
-    title: [null, Validators.compose([Validators.required, Validators.maxLength(maxLength)])],
-    category: [null, Validators.required],
-    description: [null, Validators.required],
-  });
+  addBookForm: FormGroup;
 
   categories = [
     {name: 'Drama'},
@@ -27,9 +28,23 @@ export class BookFormComponent {
 
   constructor(private fb: FormBuilder, private booksService: BookService) {}
 
-  onSubmit() {
+  ngOnInit() {
+    this.createForm();
+  }
+
+  createForm() {
+    this.addBookForm = this.fb.group({
+      title: [null, Validators.compose([Validators.required, Validators.maxLength(maxLength)])],
+      category: [null, Validators.required],
+      description: [null, Validators.required],
+    });
+  }
+
+  onSubmit(formDirective: FormGroupDirective) {
     if (this.addBookForm.valid) {
       this.booksService.addBook(this.addBookForm.value as Book);
+      formDirective.resetForm();
+      this.addBookForm.reset();
     }
   }
 }
